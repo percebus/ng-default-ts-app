b566bd6dbe4e69b85e634fc52af01bf00f41204f
Juan Guerrero <JCystems@gMail.com>
2023-06-19 15:01:30 -0500

    feat(Hello-World)

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '../routing/app-routing.module';
 import { RootComponent } from './components/root/root.component';
+import { HelloWorldComponent } from './components/hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [RootComponent],
+  declarations: [RootComponent, HelloWorldComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [RootComponent],
