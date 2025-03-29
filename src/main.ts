6a24bb8c230dd9e174c36854e19d3de52fcc2616
JCGuerrero <JCystems@gMail.com>
2025-03-29 11:37:54 -0500

    upgrade angular

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './modules/app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
